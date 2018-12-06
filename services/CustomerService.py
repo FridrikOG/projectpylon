a852d5f571debb178fdbf1f69287cb6913c5880f
Friðrik Gunnarsson <fridrik@gmx.com>
2018-12-06 11:59:40 +0000

    Adding services including input checking

--- services/CustomerService.py
+++ services/CustomerService.py
@@ -1,4 +1,5 @@
 from repositories.CustomerRepository import CustomerRepository
+import string
 
 class CustomerService:
     def __init__(self):
@@ -21,3 +22,44 @@ class CustomerService:
         
     def countingCustomers(self):
         return self.__customerRepo.countingCustomers()
+
+# Input check for the name of the customer
+    def inputNameCheck(self):
+        check = False
+        newName = ''
+        while not check:
+            fullName = input('Step 1/4 - Enter name: ').strip().title().split()
+            for name in fullName:
+                if name not in string.punctuation and name.isalpha():
+                    newName += name + ' '
+                else:
+                    print("Invalid name")
+                    self.inputNameCheck()
+            check = True
+        newName = newName.strip()
+        return newName
+
+# Input check for the age of the customer
+    def inputAgeCheck(self):
+        age = -1
+        while age < 18 or age > 80:
+            try:
+                age = int(input("Step 2/4 - Enter the age of the customer: "))
+            except:
+                print("Age has to be a number between 18 and 80 ")
+        return age
+    
+# Input check for the ssn of the customer
+    def inputSsnCheck(self):
+        ssn = ''
+        while len(str(ssn)) != 10:
+            try:
+                ssn = int(input("Step 3/4 - Enter an SSN of 10 numbers: "))
+            except ValueError:
+                print("Please enter only 10 integers")
+        return ssn
+
+# Input check for the address of the customer
+    def inputAddressCheck(self):
+        address = input("Step 4/4 - Enter address: ").capitalize()
+        return address
